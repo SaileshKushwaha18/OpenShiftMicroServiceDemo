919513d0255ab7dff6039b0f35b612ed1f3e24cb
Sailesh Kushwaha <sailesh.kushwaha18@gmail.com>
2020-02-21 01:54:43 +0530

    Update file
    Update file

--- src/main/java/com/openshift/demo/OpenShiftMicroServiceDemoApplication.java
+++ src/main/java/com/openshift/demo/OpenShiftMicroServiceDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OpenShiftMicroServiceDemoApplication {
 
 	public static void main(String[] args) {
+		
+		//TO DO
 		SpringApplication.run(OpenShiftMicroServiceDemoApplication.class, args);
 	}
 
